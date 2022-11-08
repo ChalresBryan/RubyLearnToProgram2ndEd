5cda8846fe385404ef84b4346edae977ee61c9b1
Charles <charlesbryan@Charless-MacBook-Pro.local>
2022-11-07 22:32:50 -0600

    Finshed Chapter 2

---
+++ Numbers2/AFewThingsToTry.rb
@@ -0,0 +1,27 @@
+
+# Hours in a year. How many hours are in a year?
+hoursInAYear = 24*365
+print "The amount of hours in a year are : "
+print hoursInAYear
+print "\n"
+
+# Minutes in a decade. How many minutes are in a decade?
+minutesInADecade = 60*24*365*10
+print "The amount of minutes in a decade are : "
+print minutesInADecade
+print "\n"
+
+# Your age in seconds. How many seconds old are you?
+ageInSeconds = 60*60*24*365*25
+print "My age in seconds : "
+print ageInSeconds
+print "\n"
+
+authorAge = (1025*1000*1000) / (60*60*24*365)
+print "Our dear author’s age is : "
+print authorAge
+print "\n"
+
+=begin
+mulit line comment
+=end
